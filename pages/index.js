0cb7ffad44c8e89b6257e6d06c6dac8c1c5e7b6d
aunjum <aunjum@uxd.co.jp>
2022-04-20 00:37:22 +0600

    Section-1 and Section-2 Design Setup

--- pages/index.js
+++ pages/index.js
@@ -4,190 +4,602 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Code Flambe | Free Learning Academy</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className="section-1 flex-col-hstart-vstart clip-contents">
+      <div className="rectangle-13" />
+      <img
+        src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/ljcl4tlwsd-2%3A815?alt=media&token=41bce497-9345-4b71-88c4-db5d6ca3d983"
+        alt="Not Found"
+        className="dizzy-education-1"
+      />
+      <div className="group-38 flex-col">
+        <div className="group-36 flex-col">
+          <div className="frame-24 flex-row-vstart-hstart">
+            <p className="txt-765">Learn Coding For Kids From Anywhere</p>
+          </div>
+          <p className="txt-611">
+            Discover the best coding course for your kids. Learn Coding from
+            basic.
+          </p>
         </div>
+        <div className="group-37 flex-row">
+          <div className="group-17">
+            <a className="txt-229 flex-hcenter" href='#'>Book a Class</a>
+          </div>
+          <div className="group-18">
+            <a className="txt-345 flex-hcenter" href='#'>Learn More</a>
+          </div>
+        </div>
+      </div>
+      <div className="group-781 flex-row">
+        <div className="group-35">
+          <div className="frame-32 flex-row-vend-hstart clip-contents">
+            <div className="group-236">
+              <p className="txt-295">C</p>
+            </div>
+            <a className="txt-080" href='/'>CODE FLAMBE</a>
+          </div>
+        </div>
+        <div className="frame-1 flex-row-vcenter-hcenter">
+          <div className="frame-18 flex-row-vstart-hstart">
+            <a className="txt-854 flex-hcenter" href='/'>Home</a>
+          </div>
+          <div className="frame-18 flex-row-vstart-hstart">
+            <a className="txt-8104 flex-hcenter" href='#'>Class</a>
+          </div>
+          <div className="frame-18 flex-row-vstart-hstart">
+            <a className="txt-8104 flex-hcenter" href='#'>Our Activity</a>
+          </div>
+          <div className="frame-21 flex-row-vstart-hstart">
+            <a className="txt-8104 flex-hcenter" href='#'>Contact us</a>
+          </div>
+        </div>
+        <div className="frame-181 flex-row-vcenter-hcenter">
+          <div className="frame-19 flex-row-vstart-hstart">
+            <a className="txt-191 flex-hcenter" href='#'>Login</a>
+          </div>
+        </div>
+      </div>
+      <div className="frame-26 flex-col-hstart-vstart">
+        <p className="txt-206 flex-hcenter">#Experienced Teacher</p>
+        <img
+          src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/ljcl4tlwsd-2%3A849?alt=media&token=854fbc03-52a5-4cab-b5ce-99955717f0c4"
+          alt="Not Found"
+          className="polygon-5"
+        />
+      </div>
+      <div className="group-71">
+        <div className="frame-25 flex-col-hstart-vstart">
+          <p className="txt-764 flex-hcenter">#Unique Learning</p>
+        </div>
+        <img
+          src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/ljcl4tlwsd-2%3A854?alt=media&token=cdfbb9aa-7437-4650-bc23-91aa68f86325"
+          alt="Not Found"
+          className="polygon-51"
+        />
+      </div>
+      <img
+        src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/ljcl4tlwsd-2%3A855?alt=media&token=69ea0da9-db3e-49da-9070-c6483fabc30d"
+        alt="Not Found"
+        className="pablita-element-12"
+      />
+      <img
+        src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/ljcl4tlwsd-2%3A856?alt=media&token=bc734d25-71df-4d1a-8292-53e84a129934"
+        alt="Not Found"
+        className="pablita-element-11"
+      />
+    </div>
+
+    <div className="section-2 flex-col-hstart-vstart">
+      <div className="group-32">
+        <div className="group-1075 flex-row-vcenter">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/xk05832rvbc-2%3A859?alt=media&token=7f760d90-10a7-473a-b33d-1e931b1ee079"
+            alt="Not Found"
+            className="frame-10"
+          />
+          <div className="group-31 flex-col">
+            <div className="group-19">
+              <div className="group-3">
+                <div className="frame-27 flex-row-vcenter-hstart">
+                  <p className="txt-813">
+                    Increases Logical Thinking with Coding Class
+                  </p>
+                </div>
+              </div>
+            </div>
+            <p className="txt-899">
+              Having logical thinking skills encourages kids to think for
+              themselves, to question assumptions, to develop their own
+              theories, and to test their theories against known facts. These
+              thinking skills are transferable to all situations.
+            </p>
+          </div>
+        </div>
+        <img
+          src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/xk05832rvbc-2%3A864?alt=media&token=6d219d09-0e73-41f7-bf8f-0b8734e2fed2"
+          alt="Not Found"
+          className="dizzy-gaming-1"
+        />
+      </div>
+    </div>
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
       </footer>
 
       <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+      @import url("https://fonts.googleapis.com/css2?family=Inter:wght@500;700;800&display=swap");
+      main {
+        padding: 5rem 0;
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+      }
 
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+      p {
+        margin: 0;
+      }
 
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
+      .clip-contents {
+        overflow: hidden;
+      }
 
-        footer img {
-          margin-left: 0.5rem;
-        }
+.flex-col-hstart-vstart {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+}
 
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
+.flex-col {
+  display: flex;
+  flex-direction: column;
+}
 
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
+.flex-row-vcenter-hstart {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+}
 
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
+.flex-row-vstart-hstart {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: flex-start;
+}
 
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
+.flex-row {
+  display: flex;
+  flex-direction: row;
+}
 
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
+.flex-hcenter {
+  justify-content: center;
+}
 
-        .title,
-        .description {
-          text-align: center;
-        }
+.flex-row-vend-hstart {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: flex-end;
+}
 
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
+.flex-row-vcenter {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
 
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
+.flex-row-vcenter-hcenter {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
 
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
+.section-1 {
+  position: relative;
+  background-color: rgba(17, 108, 255, 1);
+  border: 1px solid rgba(196, 196, 196, 1);
+  box-sizing: border-box;
+  width: 1366px;
+  height: 728px;
+}
 
-          max-width: 800px;
-          margin-top: 3rem;
-        }
+.section-2 {
+  padding: 157px 99px;
+  box-sizing: border-box;
+  background-color: rgba(255, 255, 255, 1);
+  width: 1366px;
+  height: 714px;
+}
 
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
+.group-32 {
+  width: 100%;
+  height: 100%;
+}
 
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
+.group-1075 {
+  width: 100%;
+  height: 100%;
+}
 
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
+.frame-10 {
+  width: 436px;
+  margin-right: 92px;
+  height: 100%;
+}
 
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
+.group-31 {
+  height: fit-content;
+}
 
-        .logo {
-          height: 1em;
-        }
+.group-19 {
+  margin-bottom: 8px;
+  box-sizing: border-box;
+  height: 140px;
+  width: 100%;
+}
 
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
+.group-3 {
+  width: 100%;
+  height: 100%;
+}
+
+.frame-27 {
+  padding: 9px 0px;
+  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
+}
+
+.txt-813 {
+  font-size: 48px;
+  font-family: Inter, sans-serif;
+  font-weight: 700;
+  line-height: 125%;
+  color: rgba(0, 35, 92, 1);
+  width: 631px;
+  word-wrap: break-word;
+}
+
+.txt-899 {
+  font-size: 16px;
+  font-family: Inter, sans-serif;
+  font-weight: 400;
+  line-height: 250%;
+  color: rgba(0, 35, 92, 1);
+  width: 562px;
+  word-wrap: break-word;
+}
+
+.dizzy-gaming-1 {
+  width: 528px;
+  height: 296.88px;
+}
+
+.rectangle-13 {
+  position: absolute;
+  top: 166px;
+  left: 748px;
+  background-color: rgba(255, 255, 255, 1);
+  width: 423px;
+  height: 395px;
+  border-radius: 20px;
+}
+
+.dizzy-education-1 {
+  position: absolute;
+  top: 160px;
+  left: 634px;
+  width: 632px;
+  height: 386.5px;
+}
+
+.group-38 {
+  position: absolute;
+  top: 204px;
+  left: 100px;
+  width: 513px;
+  height: 325px;
+}
+
+.group-36 {
+  margin-bottom: 32px;
+  box-sizing: border-box;
+  width: 100%;
+}
+
+.frame-24 {
+  margin-bottom: 16px;
+  box-sizing: border-box;
+  height: 161px;
+  width: 100%;
+}
+
+.txt-765 {
+  font-size: 50px;
+  font-family: Inter, sans-serif;
+  font-weight: 800;
+  line-height: 160%;
+  color: rgba(255, 255, 255, 1);
+  width: 523px;
+  height: 162px;
+  word-wrap: break-word;
+}
+
+.txt-611 {
+  font-size: 18px;
+  font-family: Inter, sans-serif;
+  font-weight: 500;
+  line-height: 180%;
+  color: rgba(255, 255, 255, 1);
+  width: 404px;
+  word-wrap: break-word;
+}
+
+.group-37 {
+  width: fit-content;
+}
+
+.group-17 {
+  padding: 14px 35px 11px 37px;
+  margin-right: 24px;
+  box-sizing: border-box;
+  border-radius: 10px;
+  background-color: rgba(255, 255, 255, 1);
+  width: 200px;
+  height: 100%;
+}
+
+.txt-229 {
+  font-size: 20px;
+  font-family: Inter, sans-serif;
+  font-weight: 700;
+  color: rgba(17, 108, 255, 1);
+  text-align: center;
+  word-wrap: break-word;
+}
+
+.group-18 {
+  padding: 14px 43px 11px 45px;
+  box-sizing: border-box;
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 1);
+  width: 200px;
+  height: 100%;
+}
+
+.txt-345 {
+  font-size: 20px;
+  font-family: Inter, sans-serif;
+  font-weight: 500;
+  color: rgba(255, 255, 255, 1);
+  text-align: center;
+  word-wrap: break-word;
+}
+
+.group-781 {
+  position: absolute;
+  top: 40px;
+  none: 0px;
+  width: 1166px;
+  height: 39px;
+}
+
+.group-35 {
+  padding: 0px 0.32px 0px 0px;
+  margin-right: 174px;
+  box-sizing: border-box;
+  width: 129.32px;
+  height: fit-content;
+}
+
+.frame-32 {
+  padding: 0px 3.97px 0px 0px;
+  box-sizing: border-box;
+  background-color: rgba(0, 35, 92, 1);
+  height: 100%;
+}
+
+.group-236 {
+  padding: 10px 7px 8px 10px;
+  margin-right: 9px;
+  box-sizing: border-box;
+  background-color: rgba(255, 255, 255, 1);
+  width: 36px;
+  height: 100%;
+}
+
+.txt-295 {
+  font-size: 22.55px;
+  font-family: Inter, sans-serif;
+  font-weight: 800;
+  line-height: 78%;
+  letter-spacing: 0.45px;
+  color: rgba(0, 35, 92, 1);
+  word-wrap: break-word;
+}
+
+.txt-080 {
+  font-size: 12.69px;
+  font-family: Inter, sans-serif;
+  font-weight: 800;
+  line-height: 139%;
+  letter-spacing: 0.25px;
+  color: rgba(255, 255, 255, 1);
+  word-wrap: break-word;
+}
+
+.frame-1 {
+  margin-right: 198px;
+  box-sizing: border-box;
+  height: 100%;
+}
+
+.frame-18 {
+  padding: 9px;
+  margin-right: 72px;
+  box-sizing: border-box;
+  height: 100%;
+}
+
+.txt-854 {
+  font-size: 16px;
+  font-family: Inter, sans-serif;
+  font-weight: 800;
+  color: rgba(255, 255, 255, 1);
+  text-align: center;
+  word-wrap: break-word;
+}
+
+.frame-18 {
+  padding: 9px;
+  margin-right: 72px;
+  box-sizing: border-box;
+  height: 100%;
+}
+
+.txt-8104 {
+  font-size: 16px;
+  font-family: Inter, sans-serif;
+  font-weight: 500;
+  color: rgba(255, 255, 255, 1);
+  text-align: center;
+  word-wrap: break-word;
+}
+
+.frame-21 {
+  padding: 9px;
+  box-sizing: border-box;
+  height: 100%;
+}
+
+.txt-8104 {
+  font-size: 16px;
+  font-family: Inter, sans-serif;
+  font-weight: 500;
+  color: rgba(255, 255, 255, 1);
+  text-align: center;
+  word-wrap: break-word;
+}
+
+.frame-181 {
+  height: 100%;
+}
+
+.frame-19 {
+  padding: 9px 29px;
+  box-sizing: border-box;
+  border-radius: 10px;
+  background-color: rgba(255, 255, 255, 1);
+  width: 100%;
+  height: 100%;
+}
+
+.txt-191 {
+  font-size: 16px;
+  font-family: Inter, sans-serif;
+  font-weight: 700;
+  color: rgba(17, 108, 255, 1);
+  text-align: center;
+  word-wrap: break-word;
+}
+
+.frame-26 {
+  position: absolute;
+  top: 131px;
+  left: 1003px;
+  border-radius: 20px;
+  background-color: rgba(255, 207, 45, 1);
+  width: 263px;
+  height: 60px;
+}
+
+.txt-206 {
+  position: absolute;
+  top: 18px;
+  left: 21px;
+  font-size: 20px;
+  font-family: Inter, sans-serif;
+  font-weight: 700;
+  color: rgba(0, 35, 92, 1);
+  text-align: center;
+  width: 221px;
+  height: 24px;
+  word-wrap: break-word;
+}
+
+.polygon-5 {
+  position: absolute;
+  top: 21px;
+  left: -8px;
+  width: 15px;
+  height: 17.32px;
+}
+
+.group-71 {
+  position: absolute;
+  top: 525px;
+  left: 609px;
+  width: 276.32px;
+  height: 60px;
+}
+
+.frame-25 {
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  padding: 17px 67px 16px 19px;
+  box-sizing: border-box;
+  border-radius: 20px;
+  background-color: rgba(231, 240, 255, 1);
+  width: 263px;
+  height: 100%;
+}
+
+.txt-764 {
+  font-size: 20px;
+  font-family: Inter, sans-serif;
+  font-weight: 700;
+  color: rgba(0, 35, 92, 1);
+  text-align: center;
+  word-wrap: break-word;
+}
+
+.polygon-51 {
+  position: absolute;
+  top: 11px;
+  left: 258px;
+  width: 15px;
+  height: 17.32px;
+}
+
+.pablita-element-12 {
+  position: absolute;
+  top: 158px;
+  left: 730px;
+  width: 34px;
+  height: 32.76px;
+}
+
+.pablita-element-11 {
+  position: absolute;
+  top: 544px;
+  left: 1144px;
+  width: 34px;
+  height: 32.76px;
+}
+
       `}</style>
 
       <style jsx global>{`
@@ -202,6 +614,10 @@ export default function Home() {
 
         * {
           box-sizing: border-box;
+        }
+
+        a{
+          text-decoration: none;
         }
       `}</style>
     </div>
